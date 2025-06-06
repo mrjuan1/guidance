c05a11725f4139e0931297e9049d526f81b9d8d7
Juan Wolfaardt <juanwolfaardt@gmail.com>
2025-06-06 16:32:15 +0200

    add ability to toggle power source

--- power-source.gd
+++ power-source.gd
@@ -1,4 +1,21 @@
-extends StaticBody3D
+extends CSGBox3D
+
+signal _state_toggled
+
+const _INACTIVE_COLOUR: String = "blue"
+const _ACTIVE_COLOUR: String = "yellow"
+
+var _active: bool = false
+
+@onready var _material: StandardMaterial3D = (material as StandardMaterial3D)
+@onready var _inactive_texture: Texture2D = load(_material.albedo_texture.resource_path)
+@onready var _active_texture: Texture2D = load(_material.albedo_texture.resource_path.replace(_INACTIVE_COLOUR, _ACTIVE_COLOUR))
 
 func interact() -> void:
-	print("power source clicked")
+	_active = !_active
+	if _active:
+		_material.albedo_texture = _active_texture
+	else:
+		_material.albedo_texture = _inactive_texture
+
+	_state_toggled.emit(_active)
